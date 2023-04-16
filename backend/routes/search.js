1d81f1c96c98c2eef61bd1fc4ee60cce3893a544
ffan-web <qiaofan417@gmail.com>
2023-04-16 17:48:29 -0500

    finished backend work

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -28,7 +28,41 @@ function search_all(req, res) {
         res.json(result.response);
     });
 }
+function search_keyword(req, res){
+    var keyword_movie = req.params.keyword;
+    var myStrQuery = 'q=title:"' + keyword_movie +'"&wt=json';
+
+    solrClient.search(myStrQuery, function (err, result) {
+        if (err) {
+        console.log(err);
+        res.status(500).send('Error querying Solr');
+        return;
+        }
+
+        var keyword_actor = req.params.keyword;
+        var actor_StrQuery = 'q=name:"' + keyword_actor +'"&wt=json';
+        solrClient.search(actor_StrQuery, function (err, actor) {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Error querying Solr');
+                return;
+            }
+
+
+        //console.log('Response:', result.response);
+
+        // Send the JSON response back to the client
+            res.json({
+                movies: result.response.docs,
+                actors: actor.response.docs
+            });
+        }
+        );
+    });
+
+}
 
 module.exports = (app) => {
     app.get('/search', search_all);
+    app.get('/searchkey/:keyword', search_keyword);
 }
