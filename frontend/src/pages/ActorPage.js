ab81b0d3afb81dbdbb666be9a01d52d65c4ec2a8
Fan Qiao <57175441+ffan-web@users.noreply.github.com>
2023-04-21 14:27:44 -0500

    final update actorpage and moviepage

--- frontend/src/pages/ActorPage.js
+++ frontend/src/pages/ActorPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import data from './ActorTest.json';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import './ActorPage.css';
 
 const ActorPage = (props) => {
     //const id = props.match.params.id
@@ -30,25 +31,85 @@ const ActorPage = (props) => {
         fetchMovie()
     }, [])
 
-    return <div>
-        <div class="card" style={{width: "18rem"}}>
-            < img class="card-img-top" src={actor['image']} alt=""/>
-            <div class="card-body">
-                <h5 class="card-title">{actor['name']}</h5>
-                <p class="card-text">{actor['primaryprofession']}</p >
+    const handleSearch=(event) => {
+        event.preventDefault();
+        // TODO: implement search logic using searchTerm state
+        //console.log(`Searching for "${this.state.searchTerm}"...`);
+        props.history.push(`/mainpage/${event.target[0].value}`);
+      }
+    
+
+    return <div style={{backgroundColor: "#000"}}>
+    {/* navigation bar */}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-2 mb-3">
+        {/* IMDB LOGO */}
+        <a class="navbar-brand ms-3" href="/">
+            <img class="p-0"
+            src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" width="70" height="auto" alt=""
+            style={{marginLeft: "10%"}}
+            />
+        </a>
+        {/* SEARCH BAR  align right*/}
+        <form class="d-flex ms-3 me-3 ms-auto"style = {{marginLeft: "75%"}}
+        onSubmit={(event) => (handleSearch(event))}
+        >
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            {/* buttom outline yellow */}
+            <button class="btn btn-outline-warning" type="submit">Search</button>
+        </form>
+    </nav>
+
+    
+    {/* actor info */}
+    {/* show card left alignment */}
+    <div>
+    <div class="card border-0 mb-3 text-white ms-5" style={{maxWidth: "800px",maxheight: "200px", backgroundColor: "#212121",marginLeft: "2%"}}>
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img class="card-img" src={actor['image']} alt=""/>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h2 class="card-title">{actor['name']}</h2>
+                    <p class="card-text">{actor['birthyear']} - {actor['deathyear'] ? actor['deathyear'] : "Now"}</p>
+                    <p class="card-text"><strong>Primary Profession: </strong>{actor['primaryprofession']}</p>
+                    <p class="card-text"><strong>About: </strong>William Bradley Pitt is an American actor and film producer. He is the recipient of various accolades, including two Academy Awards, two British Academy Film Awards, two Golden Globe Awards, and a Primetime Emmy Award.</p>
+                    {/* wiki page logo and link */}
+                    <a href="#">
+                        <p class="card-text">Wikipedia</p>
+                    </a>
+                </div>
             </div>
         </div>
-        {/* recommended movies */}
-        <h2>Recommended Movies</h2>
+    </div>
+    </div>
+
+
+    {/* recommended movies */}
+    <div className="recommendedBox"> 
+        <h2 className="text-left" style={{color: "#F6C700",marginLeft: "2%"}} >Recommended Movies</h2>
+        <p className="text-left" style={{color: "#F6C700",marginLeft: "2%"}}>Movies that you might like</p>
         <div className="recommended-movies container-fluid">
             <div className="d-flex flex-row flex-nowrap overflow-auto">
             {recommendations.map((movie) => {
                 return <div class="col-3">
-                        <div className="movie card" key={movie['id']} style={{width: "18rem"}}>
-                        < img class="card-img-top" src={movie['image']} alt=""/>
+                        <div className="movie card border-0 mb-3 text-white" style={{backgroundColor: "#212121",width: "18rem", height: "auto", cursor: "pointer"}} key={movie['id']}
+                        onClick={() => {window.location.href = `/moviepage/${movie['id']}`}}>
+                        <img class="card-img-top" src={movie['image']} alt="" style = {{width: "18rem", height: "24rem"}}/>
                         <div class="card-body">
                             <h5 class="card-title">{movie['title']}</h5>
-                            <p class="card-text">{movie['imDbRating']}</p >
+                            {/* show stars using rating */}
+                            <div className="stars">
+                                {[...Array(5)].map((star, i) => {
+                                    const rating = Math.floor(movie['imDbRating'] / 2);
+                                    if (i < rating) {
+                                    return <span class="star" style={{color: "#F6C700"}}>&#9733;</span>
+                                    } else {
+                                    return <span class="star" style={{color: "#F6C700"}}>&#9734;</span>
+                                    }
+                                })}
+                                <span className="rating"> {movie['imDbRating']}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -56,6 +117,8 @@ const ActorPage = (props) => {
             </div>
         </div>
     </div>
+
+</div>
 }
 
 export default ActorPage;
--- frontend/src/pages/MoviePage.js
+++ frontend/src/pages/MoviePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import "./MoviePage.css";
 
 const MoviePage = (props) => {
     const { id } = useParams()
@@ -32,29 +33,116 @@ const MoviePage = (props) => {
 
     }, [])
 
-    return <div>
-        {movie &&
-        <div className="movie">
-            <img src={movie['image']} alt="movie image" height={"20%"} width={"20%"}/>
-            <h2>{movie['title']}</h2>
+    const handleSearch=(event) => {
+        event.preventDefault();
+        // TODO: implement search logic using searchTerm state
+        //console.log(`Searching for "${this.state.searchTerm}"...`);
+        props.history.push(`/mainpage/${event.target[0].value}`);
+      }
+    
 
-        </div>}
-        <h2>recommandations</h2>
-        {recommendations &&
-        <div className="recommended-movies container-fluid">
-            <div className="d-flex flex-row flex-nowrap overflow-auto">
-            {recommendations.map((movie) => {
-                return <div className="col-3">
-                <div className="movie card" style={{width: "18rem"}}>
-                    <h3>{movie['title']}</h3>
-                    <img src={movie['image']} alt="movie image" />
-                    <p>{movie['description']}</p>
-                </div>
-                </div>
-            })}
+    return <div style={{ backgroundColor: "#000" }}>
+    {/* navigation bar */}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-2 mb-3">
+        {/* IMDB LOGO */}
+        <a class="navbar-brand ms-3" href="/">
+            <img class="p-0"
+            src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" width="70" height="auto" alt=""
+            style={{marginLeft: "10%"}}
+            />
+        </a>
+        {/* SEARCH BAR  align right*/}
+        <form class="d-flex ms-3 me-3 ms-auto" style = {{marginLeft: "75%"}}
+        onSubmit={(event) => (handleSearch(event))}
+        >
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            {/* buttom outline yellow */}
+            <button class="btn btn-outline-warning" type="submit">Search</button>
+        </form>
+    </nav>
+
+    {movie && (
+      <div
+        className="card mb-3 text-white"
+        style={{
+          maxWidth: "800px",
+          maxheight: "200px",
+          backgroundColor: "#212121",
+            marginLeft: "2%",
+        }}
+      >
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={movie["image"]}
+              className="img-fluid rounded-start"
+              alt="moive image"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h2 className="card-title mb-3">
+                {movie["title"]} {movie["description"]}
+              </h2>
+              <p className="card-text mb-3">
+                <strong>Genres:</strong> {movie["genres"]}
+              </p>
+              <div className="stars mb-3">
+              <strong>Rating: </strong>
+                                {[...Array(5)].map((star, i) => {
+                                    const rating = Math.floor(movie['imDbRating'] / 2);
+                                    if (i < rating) {
+                                    return <span class="star" style={{color: "#F6C700"}}>&#9733;</span>
+                                    } else {
+                                    return <span class="star" style={{color: "#F6C700"}}>&#9734;</span>
+                                    }
+                                })}
+                                <span className="rating"> {movie['imDbRating']}</span>
+              </div>
+              <p className="card-text mb-3">{movie["plot"]}</p>
+              <a href="#" className="btn btn-warning">
+                Watch now
+              </a>
             </div>
-        </div>}
-    </div>
+          </div>
+        </div>
+      </div>
+    )}
+
+    {recommendations && (
+      <div className="recommendedBox">
+      <h2 className="text-left" style={{color: "#F6C700",marginLeft: "2%"}}>Recommended Movies</h2>
+      <p className="text-left" style={{color: "#F6C700",marginLeft: "2%"}}>Movies that you might like</p>
+      <div className="recommended-movies container-fluid">
+          <div className="d-flex flex-row flex-nowrap overflow-auto">
+          {recommendations.map((movie) => {
+              return <div class="col-3">
+                      <div className="movie card border-0 mb-3 text-white" style={{backgroundColor: "#212121",width: "18rem", height: "auto", cursor: "pointer"}} key={movie['id']}
+                      onClick={() => {window.location.href = `/moviepage/${movie['id']}`}}>
+                      <img class="card-img-top" src={movie['image']} alt="" style = {{width: "18rem", height: "24rem"}}/>
+                      <div class="card-body">
+                          <h5 class="card-title">{movie['title']}</h5>
+                          {/* show stars using rating */}
+                          <div className="stars">
+                              {[...Array(5)].map((star, i) => {
+                                  const rating = Math.floor(movie['imDbRating'] / 2);
+                                  if (i < rating) {
+                                  return <span class="star" style={{color: "#F6C700"}}>&#9733;</span>
+                                  } else {
+                                  return <span class="star" style={{color: "#F6C700"}}>&#9734;</span>
+                                  }
+                              })}
+                              <span className="rating"> {movie['imDbRating']}</span>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          })}
+          </div>
+      </div>
+  </div>
+    )}
+  </div>
 }
 
 
